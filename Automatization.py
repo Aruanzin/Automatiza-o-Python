d89c5acd4afe54a6e27dace422e0b670de9f221c
sjohnatas7 <johnatas@usp.br>
2023-03-02 17:40:03 -0300

    main

--- Automatization.py
+++ Automatization.py
@@ -1,13 +1,26 @@
-import pandas as pd
+# import pandas as pd
 
-# Replace 'example.xlsx' with the name of your Excel file.
-df = pd.read_excel('PROGRAMAÇÃO SC - CLAUDIO.xlsx')
+# # Replace 'example.xlsx' with the name of your Excel file.
+# df = pd.read_excel('/home/johnatas/Documentos/workspace/python/Automatiza-o-Python/data.xlsx')
 
-# Define a lambda function to combine the values in four columns into a single column.
-combine_columns = lambda row: ', '.join([str(row['Column1']), str(row['Column2']), str(row['Column3']), str(row['Column4'])])
-
+# # Define a lambda function to combine the values in four columns into a single column.
+# combine_columns = df[['Cidade', 'Logradouro', 'FORNECEDOR', 'TIPO']]
+# print(combine_columns.head())
 # Apply the lambda function to each row in the DataFrame to create a new column.
-df['GroupedColumn'] = df.apply(combine_columns, axis=1)
 
 # Print the updated DataFrame to the console.
-print(df)
+
+import pandas as pd
+
+# Read the Excel file into a pandas dataframe
+df = pd.read_excel('/home/johnatas/Documentos/workspace/python/Automatiza-o-Python/data.xlsx')
+
+# Group the data by the first four columns
+dados = ['Cidade', 'Logradouro', 'FORNECEDOR', 'TIPO']
+localizacao = ['Latitude', 'Longitude']
+# Iterate over each group and concatenate the row values
+for index, row in df.iterrows():
+    conteudo = ' '.join(row[dados])
+    locationArray = [str (item) for item in row[localizacao] ]
+    loc = ' '.join(locationArray)
+    print(conteudo, loc)
