dbe30bbccf55dd657f803678cb7554c99bb53211
Aruanzin <Aruanbretas@usp.br>
2023-03-03 14:22:05 -0300

    .

--- teste.py
+++ teste.py
@@ -16,14 +16,35 @@ localizacao = ['Latitude', 'Longitude']
 options = uc.ChromeOptions()
 profile = "C:\\Users\\Usuario\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1"
 options.user_data_dir = profile
-driver = uc.Chrome(user_data_dir=profile, use_subprocess=True)
-driver.get('https://www.facebook.com/')
+driver = uc.Chrome(options=options, use_subprocess=True)
+driver.get('https://www.google.com/maps/d/u/0/edit?mid=19Af8BUv6WDvFGncBjN45gxDzWUKGeKI&usp=sharing')
+
+
+barraPesquisa = driver.find_element("id", "mapsprosearch-field")
+botaoPesquisar = driver.find_element("id", "mapsprosearch-button")
+
 
 for index, row in df.iterrows():
     sigla = row[Sigla]
     conteudo = ' '.join(row[dados])
     locationArray = [str (item) for item in row[localizacao] ]
     loc = ' '.join(locationArray)
+    barraPesquisa.send_keys(loc + "\n")
+    time.sleep(0.3)
+    greenPoint = driver.find_element("class", "un1lmc-pbTTYe-ibnC6b JIbV8-pbTTYe-ibnC6b-G0jgYd JIbV8-pbTTYe-ibnC6b-gk6SMd")
+    greenPoint.click()
+    time.sleep(0.5)
+    addToMap = driver.find_element("id", "addtomap-button")
+    addToMap.click()
+
+    #edit = driver.find_element("id", "map-infowindow-edit-button")
+    #espacoSigla = driver.find_element("id", "map-infowindow-attr-nome-value")
+    #espacoDesc = driver.find_element("id", "map-infowindow-attr-descrição-value")
+
+
+
+    
+    
 
 
 
