eab856273b10455aadad0169c3ab69094a22ece4
sjohnatas7 <johnatas@usp.br>
2023-03-04 13:27:52 -0300

    interface

--- Principal.py
+++ Principal.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-def principal(fileName):
+def principal(fileName, maps):
     # Read the Excel file into a pandas dataframe
     df = pd.read_excel(fileName)
 
@@ -20,43 +20,41 @@ def principal(fileName):
     profile = "C:\\Users\\Usuario\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1"
     options.user_data_dir = profile
     driver = uc.Chrome(options=options, use_subprocess=True)
-    driver.get('https://www.google.com/maps/d/u/0/edit?mid=19Af8BUv6WDvFGncBjN45gxDzWUKGeKI&usp=sharing')
+    driver.get(maps)
 
-    wait = WebDriverWait(driver,10)
+    # wait = WebDriverWait(driver,10)
 
-    barraPesquisa = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="mapsprosearch-field"]')))
-    botaoPesquisar = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="mapsprosearch-button"]/div')))
-
-    action_chains = ActionChains(driver)
+    # barraPesquisa = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="mapsprosearch-field"]')))
+    # botaoPesquisar = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="mapsprosearch-button"]/div')))
 
 
-    for index, row in df.iterrows():
-        sigla = row[Sigla]
-        conteudo = ' '.join(row[dados])
-        locationArray = [str (item) for item in row[localizacao] ]
-        loc = ' '.join(locationArray)
+    # for row in df.iterrows():
+    #     sigla = row[Sigla]
+    #     conteudo = ' '.join(row[dados])
+    #     locationArray = [str (item) for item in row[localizacao] ]
+    #     loc = ' '.join(locationArray)
 
-        barraPesquisa.send_keys(loc)
-        botaoPesquisar.click()
+    #     barraPesquisa.send_keys(loc)
+    #     botaoPesquisar.click()
 
-        greenPoint = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="searchresultsview"]/div/div/div[2]/div/div/div[3]')))
-        driver.execute_script("arguments[0].click()",greenPoint)
-        edit = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-edit-button"]')))
-        driver.execute_script("arguments[0].click()",edit)
+    #     greenPoint = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="searchresultsview"]/div/div/div[2]/div/div/div[3]')))
+    #     driver.execute_script("arguments[0].click()",greenPoint)
+    #     edit = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-edit-button"]')))
+    #     driver.execute_script("arguments[0].click()",edit)
 
         
 
-        espacoSigla = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-attr-nome-value"]')))
-        try:
-            espacoSigla.clear()
-        except Exception as e:
-            print(e)
+    #     espacoSigla = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-attr-nome-value"]')))
+    #     try:
+    #         espacoSigla.clear()
+    #     except Exception as e:
+    #         print(e)
 
-        espacoSigla.send_keys(sigla)
-        espacoDesc = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-attr-descrição-value"]')))
-        espacoDesc.send_keys(conteudo + " " + loc)
-        salvar = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-done-editing-button"]/div')))
-        driver.execute_script("arguments[0].click()",salvar)
+    #     espacoSigla.send_keys(sigla)
+    #     espacoDesc = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-attr-descrição-value"]')))
+    #     espacoDesc.send_keys(conteudo + " " + loc)
+    #     salvar = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="map-infowindow-done-editing-button"]/div')))
+    #     driver.execute_script("arguments[0].click()",salvar)
 
 
 
--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from Principal import principal
 
 
 window = tk.Tk()
@@ -17,7 +18,7 @@ def button_click():
     file_path = filedialog.askopenfilename(
         filetypes=(("Text files", "*.txt"), ("All files", "*.*"))
     )
-    print("Selected file:", file_path)
+    principal(fileName=file_path, maps='https://www.google.com/maps/d/u/0/edit?mid=19Af8BUv6WDvFGncBjN45gxDzWUKGeKI&usp=sharing')
 
 
 button.config(command=button_click)
