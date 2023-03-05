220f1c026b9264f3ab12e78f061eb923ba814bd4
sjohnatas7 <johnatas@usp.br>
2023-03-05 16:53:38 -0300

    improving interface

--- interface.py
+++ interface.py
@@ -6,22 +6,14 @@ def fileFinder():
     filePath = filedialog.askopenfilename()
     return filePath
 
-def on_focus_in(event):
-    if entry.get() == 'Link do mapa':
-        entry.delete(0, END)
-        entry.config(fg='black')
-
-def on_focus_out(event):
-    if entry.get() == '':
-        entry.insert(0, 'Link do mapa')
-        entry.config(fg='gray')
 
 def get_map_link(fileName, map):
     if map.startswith("https://www.google.com/maps/"):
-        try:
-            principal(fileName=fileName, map=map)
-        except Exception as e:
-            print(e)
+        # try:
+        # print(fileName)
+        principal(fileName=fileName, map=map)
+        # except Exception as e:
+        #     print(e)
     else:
         label.config(text="Link incorreto: o link do mapa deve começar com https://www.google.com/maps/")
 
@@ -32,18 +24,40 @@ window.configure(bg="#FFFFFF")
 label = tk.Label(window, text="Por favor, selecione o arquivo que você quer ler", font=('helvetica', 10, 'bold'), bg='white', pady=20)
 label.pack()
 
-entry = tk.Entry(window, width=90, relief='flat')
-entry.pack(padx=20, pady=50)
+localizacaoLabel = tk.Label(window,bg='white',width=90, text="Localização:")
+localizacaoLabel.pack(pady=20)
 
-entry.insert(0, 'Link do mapa')
-entry.bind('<FocusIn>', on_focus_in)
-entry.bind('<FocusOut>', on_focus_out)
+# Create an entry widget for the name input
+localizacaoEntry = tk.Entry(window,width=50)
+localizacaoEntry.pack()
 
-run = tk.Button(window, text="Rodar", bg='brown', fg='white', font=('helvetica', 12, 'bold'),width=10, relief="flat")
-run.pack()
+
+tituloLabel = tk.Label(window,bg='white',width=90, text="Titulo da localização:")
+tituloLabel.pack(pady=20)
+
+# Create an entry widget for the name input
+tituloEntry = tk.Entry(window,width=50)
+tituloEntry.pack()
+
+descricaoLabel = tk.Label(window,bg='white',width=90, text="Descricao da localização:")
+descricaoLabel.pack(pady=20)
+
+# Create an entry widget for the name input
+linkEntry = tk.Entry(window,width=50)
+linkEntry.pack()
+
+linkLabel = tk.Label(window,bg='white',width=90, text="Link da mapa:")
+linkLabel.pack(pady=20)
+
+# Create an entry widget for the name input
+linkEntry = tk.Entry(window,width=50 )
+linkEntry.pack()
+
+run = tk.Button(window, text="Rodar", bg='brown', fg='white', font=('helvetica', 12, 'bold'),width=42, relief="flat")
+run.pack(pady=30, padx=25)
 
 try:
-    run.config(command=lambda: get_map_link(fileName=fileFinder(), map=str(entry.get())))
+    run.config(command=lambda: get_map_link(fileName=fileFinder(), map=str(linkEntry.get())))
 except ValueError as e:
     label.config(text=str(e))
 
